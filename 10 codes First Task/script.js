254cd0660d453a0d9b06e531b31ba464f6ba3327
koppulavenkataBharathi <koppulabharathi329@gmail.com>
2024-07-03 01:37:52 +0530

    js task

---
+++ 10 codes First Task/script.js
@@ -0,0 +1,117 @@
+// console.log("1"+"4"+"1") //141
+// console.log("A"-"B")  //Nan
+// console.log("A"-"B"+"1") //NaN1
+// console.log("A"-"B"+1) //NaN
+// console.log("1"-"1") //0
+// console.log(+"1"+"1"+"1")//111
+// console.log(1 + + "1" + "1") //21
+// console.log(1 - "1" + "1") //01
+
+//1. fav foods
+//String task
+// var favFood = "biryani"; 
+// if(favFood === "biryani" || favFood === "chicken biryani" || favFood === "mutton biryani"){
+//     console.log("you are correct,my fav food is ",favFood);
+// }
+// else{
+//     console.log("you are not correct");
+// }
+
+// 2.vote task
+//Number task
+// var age = 18;
+// var gender=prompt("enter your gender");
+
+// if ((age >= 18) && (gender === "male" || gender === "Female")){
+//     console.log("you are eligible for vote");
+// }
+// else{
+//     console.log("you are not eligible for vote");
+// }
+
+// 3.boolean task
+
+// var isWorking=true;
+// if(isWorking){
+//     console.log("yes i am working");
+
+// }
+// else{
+//     console.log("Not Working");
+// }
+
+// 4.undefined task
+// var a;
+// console.log(a);
+
+// 5.Null task
+
+// var age = null;
+// if(age > 1){
+//     console.log("you are alive");
+// }
+// else if(age === 0 || age === null || age === undefined){
+//     console.log("you didn't born till now");
+// }
+// else{
+//     console.log("enter correct data");
+// }
+
+// 6.traffic light code
+
+// var trafficLight = "orange";
+// if(trafficLight == "green"){
+//     console.log("you are allowed to go");
+// }
+// else if(trafficLight == "orange"){
+//     console.log("Get ready to Go");
+// }
+// else if(trafficLight == "red"){
+//     console.log("you should stop");
+// }
+// else{
+//     console.log("Not a traffic light color");
+// }
+
+// 7.shopping offer task(gets 20 % off if shop for b/w 2000- 3000 and 10% off shop b/w 1000-2000 and 5 %off if shop b/w 500 -1000)
+// let total_bill = 1200;
+// if(total_bill > 2000 && total_bill <= 3000){
+//     console.log("you are available to 20% discount");
+// }
+// else if(total_bill > 1000  && total_bill <= 2000){
+//     console.log("you are available for 10% discount");
+// }
+// else if(total_bill > 500 && total_bill <= 1000){
+//     console.log("you are available for 5% discount");
+// }
+// else{
+//     console.log("you didn't get any discount");
+// }
+
+// 8.driving
+// var known_driving = false;
+// if(known_driving){
+//     console.log("Can travel anywhere using own vehicle");
+// }
+// else{
+//     console.log("you should choose either bus ,train , or auto");
+// }
+
+// 9.known languages
+// var language_known = prompt("enter preferred language:");
+// if(language_known == 'english' || language_known == 'telugu' || language_known == 'hindi'){
+//     console.log("Can survive in IT industry");
+// }
+// else{
+//     console.log("Please be perfect in english to get success in life");
+// }
+
+// 10.authentication code
+
+// let isVerified = false;
+// if(!isVerified){
+//     console.log("Success you are logged in");
+// }
+// else{
+//     console.log("Not Success,Try again");
+// }
