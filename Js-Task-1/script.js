0de8b817ecbe00a7105ff93e4e4e162f0d7e154e
koppulavenkataBharathi <koppulabharathi329@gmail.com>
2024-07-04 00:54:57 +0530

    task 1 added

---
+++ Js-Task-1/script.js
@@ -0,0 +1,108 @@
+// Task 1: Declare a variable with your name and display it using alert.
+// var name = "Bharathi";
+// alert(name);
+
+
+// Task 2: Declare two variables with any numeric values. Add these two variables and 
+// display the result using alert.
+// var num1 = 3;
+// var num2 = 2;
+// var sum = num1+num2;
+// alert(sum);
+
+
+// Task 3: Create a prompt to ask the user's age. If the user is 18 or older, display an alert saying "You are an adult." Otherwise, display an alert saying "You are a minor."
+
+// var age = prompt("enter your age");
+// var Age = Number(age);
+// if(Age >= 18){
+//     alert("You are adult");
+// }
+// else{
+//     alert("Your are a minor");
+// }
+
+
+// Task 4: Declare a variable with a boolean value. Use the typeof operator to check if it is a boolean. If it is, display an alert saying "This is a boolean." Otherwise, display an alert saying "This is not a boolean."
+
+// var booleanValue = true;
+// if(typeof(booleanValue) == 'boolean'){
+// alert("This is boolean value");
+// }
+// else{
+//     alert("This is not a boolean");
+// }
+
+
+// Task 5: Create a prompt to ask for a number. Multiply this number by 10 and display the result using alert.
+
+// var num = prompt("enter a num:");
+// alert(num*10); 
+
+
+// Task 6: Declare a variable with any value. Use typeof to check if the value is a string. If it is, concatenate another string to it and display the result using alert. Otherwise, display an alert saying "The value is not a string."
+
+// var str = "12";
+// if(typeof(str)==="string"){
+//     alert(str + "hello");
+// }
+// else{
+//     alert("This value is not a string");
+// }
+
+
+// Task 7: Create a prompt to ask the user's favorite number. Check if the number is even or odd using the modulus operator.r. Display an alert with the message "The number is even" or "The number is odd."
+
+
+// var favNumber = prompt("Enter your favourite number:",7);
+// if(favNumber%2 == 0){
+//    alert("Your fav number is even number");
+
+// }
+// else{
+//     alert("Your fav number is odd number");
+// }
+
+
+// Task 8: Declare a variable with a number. Use conditional branching to check if the number is positive, negative, or zero. Display an alert with the appropriate message.
+
+// var number = 5;
+// if(number>0){
+//     alert("Positive");
+// }
+// else if(number<0){
+//     alert("Negative");
+// } 
+// else if (number === 0){
+//     alert("Zero");
+// }
+// else{
+//     alert("It is not a number");
+// }
+
+
+// Task 9: Create a prompt to ask the user's name. If the name is empty, display an alert saying "Name cannot be empty." Otherwise, display an alert with the message "Hello, [name]!
+
+// var Name = prompt("enter your name:");
+// if(Name === ""){
+//     alert("Name cannot be empty");
+// }
+// else{
+//     alert(`hello ${Name}`);
+// }
+
+
+
+// Task 10: Declare two variables with numeric values. Use the comparison operators to check if the first number is greater than, less than, or equal to the second number. Display the appropriate message using alert
+
+// var a = 4;
+// var b = 3;
+// if(a>b){
+//     alert(`a: ${a} is greater than b: ${b}`);
+// }
+// else if(a<b){
+//     alert(`a: ${a} is less than b: ${b}`);
+// }
+// else{
+//     alert(`a: ${a} is equal than b: ${b}`);
+// }
